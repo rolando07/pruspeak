5b21637c1781f045d8f6f3adc6341ede70eba31f
Rolando Correa <rolando07@gmail.com>
2015-03-02 17:20:37 -0600

    Update pru0_firmware.h

--- src/pru-firmware/pru0_firmware.h
+++ src/pru-firmware/pru0_firmware.h
@@ -78,7 +78,7 @@ extern void sc_downcall(int (*handler)(u32 nr, u32 arg0, u32 arg1, u32 arg2, u32
 #define SET_PWM_b       5
 #define SET_PWM_c       6
 #define SET_AO_a       	7
-#define SET_AI_a       	8
+#define SET_AO_b       	8
 
 /*To be implmented
 #define SET_AIO_a     x (Replace with an available upcode) 
