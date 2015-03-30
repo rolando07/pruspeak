906332c9e313bac31567cb08c0e7b9fc5741f8c5
Rolando Correa <rolando07@gmail.com>
2015-03-30 16:45:56 -0500

    Update pru0_firmware.c
    Reverted the last two changes on GET

--- src/pru-firmware/pru0_firmware.c
+++ src/pru-firmware/pru0_firmware.c
@@ -433,8 +433,8 @@ void wait_goto_get_handler(int opcode, u32 inst)
 	else if (opcode == GOTO)
 		inst_pointer = val;
 
-	//else if((opcode == GET) && single_command)
-		//send_ret_value(val);	
+	else if((opcode == GET) && single_command)
+		send_ret_value(val);	
 }
 
 #if 1
@@ -891,7 +891,7 @@ void execute_instruction()
 	
 		case WAIT:
 		case GOTO:
-		//case GET:
+		case GET:
 			wait_goto_get_handler(opcode, inst);
 		break;
 
