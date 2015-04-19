7c2d4f21682f0593581e12b06eabc4cf7d652bcc
Rolando Correa <rolando07@gmail.com>
2015-04-19 14:31:53 -0500

    Update pru0_firmware.h

--- src/pru-firmware/pru0_firmware.h
+++ src/pru-firmware/pru0_firmware.h
@@ -79,9 +79,11 @@ extern void sc_downcall(int (*handler)(u32 nr, u32 arg0, u32 arg1, u32 arg2, u32
 #define SET_PWM_a       4
 #define SET_PWM_b       5
 #define SET_PWM_c       6
-#define GET_AI_a       	7
-#define GET_AI_b       	8
-#define GET_AI_c 	9
+
+/*GET RES[x] - Analog Input operation */
+#define GET_AI       	7
+#define GET_AI       	8
+#define GET_AI 		9
 
 /*To be implmented
 #define SET_AIO_a     x (Replace with an available upcode) 
